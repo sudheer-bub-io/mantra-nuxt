7e236777b1e83ceb8257e62f1d6e3f26f0a6dfc2
Sudheer <sudheerjanga9999@gmail.com>
2024-01-25 18:49:04 +0530

    changed image address

--- pages/quote.vue
+++ pages/quote.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex w-6/12 h-8/12 bg-slate-300 flex justify-center ml-96 mt-20 relative">
       <h1 class="absolute top-0 left-30 text-white z-10 text-3xl text-blue-950">Quote Of The Day</h1>
-      <img src="`@/assets/images/quote1.jpg`" class="w-12/12 z-0" alt="Random Quote Image">
+      <img src="../assets/images/quote1.jpg" class="w-12/12 z-0" alt="Random Quote Image">
       <h2 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white z-10 text-2xl text-blue-950">
         {{ quotes.quote }}
       </h2>
