6425c708c04173faef952706481868da7d005882
Sudheer <sudheerjanga9999@gmail.com>
2024-01-31 09:46:14 +0530

    modified plugin

--- plugins/toast.js
+++ plugins/toast.js
@@ -1,5 +1,5 @@
 import Toast from 'vue-toastification';
-import 'vue-toastification/dist/index.css';
+//import 'vue-toastification/dist/index.css';
 
 export default defineNuxtPlugin((app) => {
   app.vueApp.use(Toast, {
