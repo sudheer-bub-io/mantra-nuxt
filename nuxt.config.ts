25cb49de9c9df7e7fc6c061b06c841d2c15edbcd
Sudheer <sudheerjanga9999@gmail.com>
2024-01-31 10:41:12 +0530

    updated nuctjs.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', "@nuxt/image"],
   buildDir: 'build',
+  build: {
+     transpile: ['vue-toastification']
+     }
 });
